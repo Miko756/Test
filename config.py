62c1b717ea99d0dba339d696d637b5137943b528
Yae Miko <164617880+Miko756@users.noreply.github.com>
2024-11-02 00:05:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,10 @@
 from operator import add
 import os
 import logging
-
-
 # import dotenv
 # dotenv.load_dotenv()
-
-
-
 from logging.handlers import RotatingFileHandler
 
-#force user to join your backup channel leave 0 if you don't need.
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
 
@@ -18,45 +12,29 @@ if FORCE_SUB_CHANNEL > FORCE_SUB_CHANNEL2:
     temp = FORCE_SUB_CHANNEL2 
     FORCE_SUB_CHANNEL2 = FORCE_SUB_CHANNEL
     FORCE_SUB_CHANNEL = temp
-
-#bot stats
+    
 BOT_STATS_TEXT = os.environ.get("BOTS_STATS_TEXT","<b>BOT UPTIME </b>\n{uptime}")
-#send custom message when user interact with bot
 USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "Don't send me messages directly I'm only File Share bot! ")
 
-#your bot token here from https://telegram.me/BotFather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7716433955:AAHR3r3qgAIsf7xOmCbAp42VCIkiKljmyxg") 
-#your api id from https://my.telegram.org/apps
 APP_ID = int(os.environ.get("APP_ID", "22469064"))
-#your api hash from https://my.telegram.org/apps
 API_HASH = os.environ.get("API_HASH", "c05481978a217fdb11fa6774b15cba32")
-#your channel_id from https://t.me/MissRose_bot by forwarding dummy message to rose and applying command `/id` in reply to that message
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002381050327"))
-#your id of telegram can be found by https://t.me/MissRose_bot with '/id' command
 OWNER_ID = int(os.environ.get("OWNER_ID", "6768137528"))
-#port set to default 8080
 PORT = os.environ.get("PORT", "6666")
-#your database url mongodb only You can use mongo atlas free cloud database
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://rohitplayer87089:rohit870@cluster0.4wt927p.mongodb.net/?retryWrites=true&w=majority")
-#your database name
 DB_NAME = os.environ.get("DB_NAME", "filestorabot")
-
-#for creating telegram thread for bot to improve performance of the bot
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-#your start default command message.
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link. ")
-#your telegram tag without @
 OWNER_TAG = os.environ.get("OWNER_TAG", "Alya_x_Yuki")
+
 #Time in seconds for message delete, put 0 to never delete
 TIME = int(os.environ.get("TIME", "57600"))
-
-
 #Shortner (token system) 
 """
 some token verification sites
 https://dashboard.shareus.io/
 """
-
 # Turn this feature on or off using True or False put value inside  ""
 # TRUE for yes FALSE if no 
 USE_SHORTLINK = True if os.environ.get('USE_SHORTLINK', "TRUE") == "TRUE" else False 
